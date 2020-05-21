bfa2c1bd69b3cf6a17e235f9c150f83616c9e695
Marcell Schuh <marcell.schuh@gmail.com>
2020-05-21 10:45:22 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # iTerm2WinTerm
+
+Simple python script to convert iTerm color schemes into Windows Terminal compatible JSON.
+
+## Usage
+
+    ```bash
+    ```
---
+++ iTerm2WinTerm.py
@@ -0,0 +1,92 @@
+import plistlib
+import json
+import re
+from sys import argv
+
+def itermKeyToWinKey(iterm_key):
+    itermToWinDict = {
+        'Ansi 0 Color'    : 'black',
+        'Ansi 1 Color'    : 'red',
+        'Ansi 2 Color'    : 'green',
+        'Ansi 3 Color'    : 'yellow',
+        'Ansi 4 Color'    : 'blue',
+        'Ansi 5 Color'    : 'purple',
+        'Ansi 6 Color'    : 'cyan',
+        'Ansi 7 Color'    : 'white',
+        'Ansi 8 Color'    : 'brightBlack',
+        'Ansi 9 Color'    : 'brightRed',
+        'Ansi 10 Color'   : 'brightGreen',
+        'Ansi 11 Color'   : 'brightYellow',
+        'Ansi 12 Color'   : 'brightBlue',
+        'Ansi 13 Color'   : 'brightPurple',
+        'Ansi 14 Color'   : 'brightCyan',
+        'Ansi 15 Color'   : 'brightWhite',
+        'Background Color': 'background',
+        'Foreground Color': 'foreground',
+    }
+    try:
+        return itermToWinDict[iterm_key]
+    except:
+        return 'undefined'
+
+def rgbToHex(r, g, b):
+    return ('#%02x%02x%02x' % (int(r * 255), int(g * 255), int(b * 255))).upper()
+
+def printUsage():
+    print("usage: iTerm2WinTerm.py <iTerm color scheme> [--out <output JSON location>]")
+    exit(0)
+
+itermColorsRegex = re.compile('(.+).itermcolors')
+jsonRegex = re.compile('(.+).json')
+
+def checkArgs():
+    if len(argv) != 2 and len(argv) != 4:
+        printUsage()
+    if itermColorsRegex.fullmatch(argv[1]) == None:
+        printUsage()
+    if len(argv) == 4 and argv[2] != '--out':
+        printUsage()
+
+def main():
+    checkArgs()
+
+    print(argv)
+
+    itermFileName = argv[1]
+    schemeName = itermColorsRegex.fullmatch(itermFileName).group(1)
+
+    itermFile = ''
+    itermScheme = ''
+    try:
+        itermFile = open(itermFileName, "rb")
+        itermScheme = plistlib.load(itermFile, fmt = plistlib.FMT_XML)
+    except IOError:
+        print("Cannot find specified iTerm colors file")
+        printUsage()
+    except:
+        print("Error parsing {}".format(itermFileName))
+        printUsage()
+    outputDict = {'name': schemeName}
+
+    for key in itermScheme:
+        winKey = itermKeyToWinKey(key)
+        colorDict = itermScheme[key]
+        if winKey != 'undefined':
+            outputDict[winKey] = rgbToHex(colorDict['Red Component'], colorDict['Green Component'], colorDict['Blue Component'])
+    
+    prettyJson = json.dumps(outputDict, indent=4)
+    if len(argv) == 4:
+        outFileName = argv[3]
+        if jsonRegex.fullmatch(outFileName) == None: outFileName += '.json'
+        try:
+            jsonFile = open(outFileName, 'w')
+            jsonFile.write(prettyJson)
+            jsonFile.close()
+        except:
+            print("Error while writing to {}".format(outFileName))
+    else:
+        print(prettyJson)
+
+
+if __name__ == '__main__':
+    main()
